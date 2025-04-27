835511b54897337e9229df5aa3bc37fbc5c8b819
dabilci <havvaduman007@gmail.com>
2025-04-27 21:27:26 +0300

    CSE224Prıoject
    CSE224_Project

--- twos_complement/config.tcl
+++
@@ -1,6 +0,0 @@
-set ::env(DESIGN_NAME) twos_complement
-set ::env(VERILOG_FILES) [glob $::env(DESIGN_DIR)/src/twos_complement.v]
-set ::env(CLOCK_PORT) ""
-set ::env(CLOCK_PERIOD) ""
-
-set ::env(FP_CORE_UTIL) 50
--- twos_complement/src/twos_complement.v
+++ twos_complement/src/twos_complement.v
@@ -1,8 +1,29 @@
-module twos_complement(
-    input [7:0] in,  
-    output [7:0] out  
-);
-    
-    assign out = ~in + 1; 
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 16.03.2025 16:26:06
+// Design Name: 
+// Module Name: twos_complement
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
 
+
+module twos_complement(
+    input [7:0] inp,
+    output [7:0] outp
+);
+    assign outp = ~inp + 1'b1;
 endmodule
+
